cf3d825fea2656bc8bc0e8e5beca651f6dfaaa3e
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-08-08 15:29:32 +0530

    Update page.c

--- EASY/page.c
+++ EASY/page.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
 
+void add(int value, int arr[], int len){
+	for(int i =0; i< len; i++){
+		if (arr[i] == -1){
+			arr[i] = value;
+			return;
+		}
+	}
+	arr[0] = value;
+}
+
 int main() {
 	int f;
-	printf("Enter no of Frames");
+	printf("Enter no of Frames ");
 	scanf("%d", &f);
 	
-	int frames[f] = {-1};
-	printf("Enter no of pages");
+	int frames[f];
+	for(int i=0; i < f; i++) frames[i] = -1;
 	
 	int p;
-	printf("Enter no of pages");
+	printf("Enter no of pages ");
 	scanf("%d", &p);
 	
 	int page[p];
-	printf("Enter pages");
+	printf("Enter Pages\n");
 	for(int i=0; i < p; i++) scanf("%d", &page[i]);
 	
-	printf("%d %d", page[1],frames[1] );
 	
 	for(int i=0; i < p; i++) {
 		int avail = 0;
 		
 		printf("%d\t", page[i]);
 		for(int x=0; x < f; x++) {
-			frames[x] == -1 ? printf(" - ") : printf(" %d ",frames[i]);
-			if (frames[x] == page[i]){
-				avail = 1;
-				printf(
-			}
-		}	
+			int temp = frames[x];
+			if (temp == page[i]) avail = 1;
+			
+		}
+		if (avail == 1) printf("Hit ");	
+		else {
+			add(page[i], frames, f);
+			printf("Miss ");		
+		}
+		for(int x=0; x < f; x++) {
+			int temp = frames[x];
+			temp == -1 ? printf(" - ") : printf(" %d ",temp);
+		}
+		printf("\n");	
 	}
 }
