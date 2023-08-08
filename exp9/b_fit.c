6839112bf1a8ddee914267c1a552829b1535428b
JOWIN JOSEPH RAJU <98506673+JOWIN-JOSEPH-RAJU@users.noreply.github.com>
2023-08-08 20:43:15 +0530

    function based b_fit.c

--- exp9/b_fit.c
+++ exp9/b_fit.c
@@ -1,47 +1,50 @@
-#include<stdio.h>
-#define MAX 20
+#include <stdio.h>
+
+
+void bestfit(int no_block,int no_process,int block[],int process[])
+{
+   int n,i,j;
+   for(i=0;i<no_process;i++)
+      {
+         n=-1;
+         for(j=0;j<no_block;j++)
+            {
+             if(block[j]>=process[i])
+                {
+                  if(n==-1||block[n]>=block[j])
+                    {
+                      n=j;
+                     }
+                 }  
+             }
+   
+     printf("%d\t%d\t%d\t",n+1,process[i],block[n]);
+     block[n]=block[n]-process[i];
+     printf("%d\n",block[n]);
+    }
+}
+
+
 int main()
 {
-	int bsize[MAX],fsize[MAX],nb,nf;
-	int temp,low=10000;
-	static int bflag[MAX],fflag[MAX];
-	int i,j;
-	printf("\n enter the number of blocks");
-	scanf("%d",&nb);
-	for(i=1;i<=nb;i++)
-	{
-		printf("Enter the size of memory block % d",i);
-		scanf("%d", &bsize[i]);
-	}
-	printf("\n enter the number of files");
-	scanf("%d",&nf);
-	for(i=1;i<=nf;i++)
-	{
-		printf("\n enetr the size of file %d",i);
-		scanf("%d",&fsize[i]);
-	}
-	for(i=1;i<=nf;i++)
-	{
-		for(j=1;j<=nb;j++)
-		{
-			if(bflag[j]!=1)
-			{
-				temp=bsize[j]-fsize[i];
-				if(temp>=0)
-				{
-					if(low>temp)
-					{
-						fflag[i]=j;
-						low=temp;
-					}
-				}
-			}
-		}
-		bflag[fflag[i]]=1;
-		low=10000;
-	}
-	printf("\n file no \t file.size\t block no \t block size");
-	for(i=1;i<=nf;i++)
-		printf("\n \n %d \t\t%d\t\t%d\t\t%d",i,fsize[i],fflag[i],bsize[fflag[i]]);
-	printf("\n")
+int block_array[30],process_array[30],nblock,nprocess,i;
+printf("enter the number of block\n");
+scanf("%d",&nblock);
+printf("enter the size of blocks\n");
+for(i=0;i<nblock;i++)
+{
+scanf("%d",&block_array[i]);
+}
+
+
+printf("enter the number of process\n");
+scanf("%d",&nprocess);
+printf("enter the size of process\n");
+for(i=0;i<nprocess;i++)
+{
+scanf("%d",&process_array[i]);
+}
+printf("blocks\tsize\tmemory\tremaining\n");
+bestfit(nblock,nprocess,block_array,process_array);
+
 }
